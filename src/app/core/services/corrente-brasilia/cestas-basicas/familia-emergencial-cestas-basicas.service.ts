2d23e02e2e96afc965b20f975f40a2b648f45011
F9901059 Wilson Ricardo Martins Lopes <wrmlopes@bb.com.br>
2020-06-18 20:47:32 -0300

    novo serviço de carregamento de dados

--- src/app/core/services/corrente-brasilia/cestas-basicas/familia-emergencial-cestas-basicas.service.ts
+++ src/app/core/services/corrente-brasilia/cestas-basicas/familia-emergencial-cestas-basicas.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { CestaBasica } from 'src/app/shared/models/cesta-basica';
 import { Observable } from 'rxjs';
+import { FamiliaEmergencial } from 'src/app/shared/models/familia-emergencial';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,13 @@ export class FamiliaEmergencialCestasBasicasService {
 
     return this.http
       .get<CestaBasica[]>(
-        this.cestasBasicasDaFamiliaEmergencialService + '/' + codFamilia + '/cestabasicas');
+        `${this.cestasBasicasDaFamiliaEmergencialService}/${codFamilia}/cestabasicas`);
+  }
+
+  recuperarFamiliaEmergencialECestas(): Observable<FamiliaEmergencial[]> {
+
+    return this.http
+      .get<CestaBasica[]>(
+        `${this.cestasBasicasDaFamiliaEmergencialService}/cestasbasicas`);
   }
 }
